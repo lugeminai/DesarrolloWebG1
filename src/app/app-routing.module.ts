7a58f62626e4013291eaeebf1d2b18309051dc76
unknown <gerencia@cl-concept.com>
2021-01-25 21:40:08 -0500

    Primer Commit segun explicacion en chat

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { TeamComponent } from './pages/team/team.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectComponent } from './pages/project/project.component';
+import { ApartmentComponent } from './pages/apartment/apartment.component';
+
+const routes: Routes = [
+    { path: 'apartment/:id', component: ApartmentComponent },
+    { path: 'project/:id', component: ProjectComponent },
+    { path: 'project', component: ProjectComponent },
+    { path: 'contactus', component: ContactusComponent },
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: 'team', component: TeamComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { TeamComponent } from './pages/team/team.component';
+import { HomeComponent } from './pages/home/home.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ProjectComponent } from './pages/project/project.component';
+import { ApartmentComponent } from './pages/apartment/apartment.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactusComponent,
+    NosotrosComponent,
+    TeamComponent,
+    HomeComponent,
+    ProjectComponent,
+    ApartmentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
