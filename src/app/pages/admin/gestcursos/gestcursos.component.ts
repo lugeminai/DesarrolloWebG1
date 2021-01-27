7cc0cad0bdca7bbfcd01bd4ca5927b314c33db4f
Lugeminai <lugemi@gmail.com>
2021-01-27 09:13:43 -0500

    Create gestcursos.component.ts

---
+++ src/app/pages/admin/gestcursos/gestcursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestcursos',
+  templateUrl: './gestcursos.component.html',
+  styleUrls: ['./gestcursos.component.css']
+})
+export class GestcursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
