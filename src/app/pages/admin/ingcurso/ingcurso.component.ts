106ef6e632dc0582958372e262c1f42ad74ba5a0
Lugeminai <lugemi@gmail.com>
2021-01-27 19:58:07 -0500

    Create ingcurso.component.ts

---
+++ src/app/pages/admin/ingcurso/ingcurso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingcurso',
+  templateUrl: './ingcurso.component.html',
+  styleUrls: ['./ingcurso.component.css']
+})
+export class IngcursoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
